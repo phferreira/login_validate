91104b91dac3597b3204b71791cbb4e22e319949
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-21 13:33:07 -0300

    Implements usecase calls on function

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,5 +1,15 @@
+import 'package:dart_login/modules/connection/domain/usecases/implementation/postgres_connection.dart';
+import 'package:dart_login/modules/query/domain/usecases/implementation/query_usecase.dart';
+import 'package:dart_login/modules/query/external/datasources/query_datasource.dart';
+import 'package:dart_login/modules/query/infrastructure/repositories/query_repository.dart';
 import 'package:functions_framework/functions_framework.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
-Response function(Request request) => Response.ok('Hello, World!');
+Future<Response> function(Request request) async {
+  final usecase = QueryUsecase(repository: QueryRepository(dataSource: QueryDataSource(connection: PostgresConnection())));
+
+  var result = await usecase('select 2 from pg_catalog.pg_stat_activity limit 2;');
+
+  return Response.ok(result.toString());
+}
