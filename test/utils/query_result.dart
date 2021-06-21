973b804ee7b8eeff7780b0d2f17583861a7f4066
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-21 13:10:40 -0300

    Included request and response for test

---
+++ test/utils/query_result.dart
@@ -0,0 +1,5 @@
+import 'package:dart_login/core/utils/types/query_type.dart';
+
+final String queryConsult = 'select 1 as result from pg_catalog.pg_stat_activity limit 1;';
+
+final QueryType queryResult = ['1'];
