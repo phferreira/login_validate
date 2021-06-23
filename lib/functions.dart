e98ba7bb0a49b620683594b5aa475807a51f846e
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-23 18:01:58 -0300

    Refactoring the method, return a response ok or forbidden depending on the return of LoginValidateUseCase, the request must be a string of base64

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,15 +1,13 @@
-import 'package:dart_login/modules/connection/domain/usecases/implementation/postgres_connection.dart';
-import 'package:dart_login/modules/query/domain/usecases/implementation/query_usecase.dart';
-import 'package:dart_login/modules/query/external/datasources/query_datasource.dart';
-import 'package:dart_login/modules/query/infrastructure/repositories/query_repository.dart';
+import 'dart:convert';
+import 'package:dart_login/modules/login_validate/domain/usecases/implementations/login_validate_usecase.dart';
+import 'package:dart_login/modules/login_validate/externals/datasources/login_validate_datasource.dart';
+import 'package:dart_login/modules/login_validate/infrastructures/repositories/login_validate_repository.dart';
 import 'package:functions_framework/functions_framework.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
 Future<Response> function(Request request) async {
-  final usecase = QueryUsecase(repository: QueryRepository(dataSource: QueryDataSource(connection: PostgresConnection())));
-
-  var result = await usecase('select 2 from pg_catalog.pg_stat_activity limit 2;');
-
-  return Response.ok(result.toString());
+  final usecase = LoginValidateUseCase(repository: LoginValidateRepository(datasource: LoginValidateDataSource()));
+  var result = await usecase(await request.readAsString());
+  return result.fold((l) => Response.forbidden(l.message), (r) => Response.ok(jsonEncode(r)));
 }
