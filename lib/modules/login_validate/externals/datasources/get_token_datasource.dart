83d4879941973d5ab0fb9f50f11782b1ed80fb33
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 17:21:54 -0300

    Included datasource getToken, must be adjust the url

---
+++ lib/modules/login_validate/externals/datasources/get_token_datasource.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:dart_login/core/error/failure.dart';
+import 'package:dart_login/core/utils/types/json_types.dart';
+import 'package:dart_login/modules/login_validate/infrastructures/datasources/i_get_token_datasource.dart';
+
+class GetTokenDataSource extends IGetTokenDatasource {
+  final dio = Dio();
+
+  @override
+  Future<Either<Failure, JsonType>> getToken(JsonResultType param) async {
+    var response = (await dio.post('http://localhost:4323', data: param));
+    return Right(response.data);
+  }
+}
