d908661bc47a4b37dfd24d5b4053ffcb527bd932
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 17:20:10 -0300

    Altered the return type to JsonType

--- lib/modules/login_validate/domain/usecases/implementations/login_validate_usecase.dart
+++ lib/modules/login_validate/domain/usecases/implementations/login_validate_usecase.dart
@@ -12,7 +12,7 @@ class LoginValidateUseCase extends ILoginValidateUseCase {
   });
 
   @override
-  Future<Either<Failure, JsonResultType>> call(JsonType param) {
+  Future<Either<Failure, JsonType>> call(JsonType param) {
     return repository.loginValidate(param);
   }
 }
--- lib/modules/login_validate/domain/usecases/login_validate_usecase.dart
+++ lib/modules/login_validate/domain/usecases/login_validate_usecase.dart
@@ -3,7 +3,7 @@ import 'package:dart_login/core/usecase/usecase.dart';
 import 'package:dart_login/core/utils/types/json_types.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class ILoginValidateUseCase extends IUseCase<JsonResultType, JsonType> {
+abstract class ILoginValidateUseCase extends IUseCase<JsonType, JsonType> {
   @override
-  Future<Either<Failure, JsonResultType>> call(JsonType param);
+  Future<Either<Failure, JsonType>> call(JsonType param);
 }
