8017ea8618504427cdf0133325e872ab6105fdcc
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 17:27:30 -0300

    Add getToken to usecase

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'package:dart_login/modules/connection/domain/usecases/implementation/postgres_connection.dart';
 import 'package:dart_login/modules/login_validate/domain/usecases/implementations/login_validate_usecase.dart';
+import 'package:dart_login/modules/login_validate/externals/datasources/get_token_datasource.dart';
 import 'package:dart_login/modules/login_validate/externals/datasources/login_validate_datasource.dart';
 import 'package:dart_login/modules/login_validate/infrastructures/repositories/login_validate_repository.dart';
 import 'package:functions_framework/functions_framework.dart';
@@ -7,7 +9,12 @@ import 'package:shelf/shelf.dart';
 
 @CloudFunction()
 Future<Response> function(Request request) async {
-  final usecase = LoginValidateUseCase(repository: LoginValidateRepository(datasource: LoginValidateDataSource()));
+  final usecase = LoginValidateUseCase(
+    repository: LoginValidateRepository(
+      datasource: LoginValidateDataSource(),
+      getToken: GetTokenDataSource(),
+    ),
+  );
   var result = await usecase(await request.readAsString());
   return result.fold((l) => Response.forbidden(l.message), (r) => Response.ok(jsonEncode(r)));
 }
