117b3b423a3c72180314db35536bfad097e045c1
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 18:48:36 -0300

    Adjusted resultQuery to return default

--- test/utils/query_result.dart
+++ test/utils/query_result.dart
@@ -2,4 +2,8 @@ import 'package:dart_login/core/utils/types/query_type.dart';
 
 final String queryConsult = 'select 1 as result from pg_catalog.pg_stat_activity limit 1;';
 
-final QueryType queryResult = ['1'];
+final QueryType queryResult = [
+  {
+    'tableName': {'columnName': '1'}
+  }
+];
