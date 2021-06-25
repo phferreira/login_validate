cbed47b8a5e87aac61a35f6160a41b9e49343099
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 17:26:31 -0300

    Add getToken dependency and use to validate login, case valid login then call the getToken

--- lib/modules/login_validate/infrastructures/repositories/login_validate_repository.dart
+++ lib/modules/login_validate/infrastructures/repositories/login_validate_repository.dart
@@ -1,18 +1,23 @@
-import 'package:dart_login/modules/login_validate/domain/repositories/login_validate_repository.dart';
-import 'package:dart_login/modules/login_validate/infrastructures/datasources/login_validate_datasource.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dart_login/core/error/failure.dart';
 import 'package:dart_login/core/utils/types/json_types.dart';
+import 'package:dart_login/modules/login_validate/domain/repositories/login_validate_repository.dart';
+import 'package:dart_login/modules/login_validate/infrastructures/datasources/i_get_token_datasource.dart';
+import 'package:dart_login/modules/login_validate/infrastructures/datasources/i_login_validate_datasource.dart';
 
 class LoginValidateRepository extends ILoginValidateRepository {
   ILoginValidateDataSource datasource;
+  IGetTokenDatasource getToken;
 
   LoginValidateRepository({
     required this.datasource,
+    required this.getToken,
   });
 
   @override
-  Future<Either<Failure, JsonResultType>> loginValidate(JsonType param) async {
-    return await datasource.loginValidate(param);
+  Future<Either<Failure, JsonType>> loginValidate(JsonType param) async {
+    return (await datasource.loginValidate(param)).fold((l) => Left(l), (r) async {
+      return (await getToken.getToken(r)).fold((l) => Left(l), (r) => Right(r));
+    });
   }
 }
