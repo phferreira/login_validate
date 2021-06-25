a63d08fa2302c1ae5fdbaeb51e348a754c8f3adc
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-25 17:23:29 -0300

    Adjust the name of login_valodate_datasoure

--- lib/modules/login_validate/externals/datasources/login_validate_datasource.dart
+++ lib/modules/login_validate/externals/datasources/login_validate_datasource.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:dart_login/core/utils/types/json_types.dart';
 import 'package:dart_login/core/error/failure.dart';
 import 'package:dart_login/modules/connection/domain/errors/failure.dart';
 import 'package:dart_login/modules/connection/domain/usecases/implementation/postgres_connection.dart';
 import 'package:dart_login/modules/login_validate/domain/errors/failures.dart';
-import 'package:dart_login/modules/login_validate/infrastructures/datasources/login_validate_datasource.dart';
+import 'package:dart_login/modules/login_validate/infrastructures/datasources/i_login_validate_datasource.dart';
 import 'package:dart_login/modules/query/domain/usecases/implementation/query_usecase.dart';
 import 'package:dart_login/modules/query/external/datasources/query_datasource.dart';
 import 'package:dart_login/modules/query/infrastructure/repositories/query_repository.dart';
